4fde82a0fc5137cfaf45eeae861d2bdca15a37db
Marc Oller <marc.om@icloud.com>
2020-10-30 12:49:37 +0100

    Fix header and add style

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,1 @@
 console.log('this works!');
-
-window.onscroll = function() {myFunction()};
-
-let header = document.getElementById("myHeader");
-let sticky = header.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
