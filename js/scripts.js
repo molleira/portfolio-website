85eb59ade41ef55dd17300d034aea855f2a55cb9
Marc Oller <marc.om@icloud.com>
2020-11-13 14:18:40 +0100

    Start hamburger menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,22 @@
 console.log('this works!');
+
+let toggle = document.querySelector(".toggle");
+let menu = document.querySelector(".menu");
+ 
+/* Toggle mobile menu */
+function toggleMenu() {
+    if (menu.classList.contains("active")) {
+        menu.classList.remove("active");
+         
+        // adds the menu (hamburger) icon
+        toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
+    } else {
+        menu.classList.add("active");
+         
+        // adds the close (x) icon
+        toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
+    }
+}
+ 
+/* Event Listener */
+toggle.addEventListener("click", toggleMenu, false);
