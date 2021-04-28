151d80e347fff46df2313f32a75987f9ddf5fe3b
Marc Oller <marc.om@icloud.com>
2021-04-28 12:39:10 +0200

    Clean code - scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,83 +1,55 @@
-// console.log('this works!');
-
-// let toggle = document.querySelector(".toggle");
-// let menu = document.querySelector(".menu");
-
-// /* Toggle mobile menu */
-// function toggleMenu() {
-//     if (menu.classList.contains("active")) {
-//         menu.classList.remove("active");
-
-//         // adds the menu (hamburger) icon
-//         toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
-//     } else {
-//         menu.classList.add("active");
-
-//         // adds the close (x) icon
-//         toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
-//     }
-// }
-
-/* Event Listener */
-// toggle.addEventListener("click", toggleMenu, false);
-
+// selects hamburger and nav links and assigns to variable
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link')
 
+// opens nav when click
 navToggle.addEventListener('click', () => {
   document.body.classList.toggle('nav-open');
 });
 
+// closes nav when click on nav link
 navLinks.forEach(link => {
   link.addEventListener('click', () => {
     document.body.classList.remove('nav-open');
   })
 })
 
-// go to top button
-//Get the button:
-// mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-// window.onscroll = function () { scrollFunction() };
-
-// function scrollFunction() {
-//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//     mybutton.style.display = "block";
-//   } else {
-//     mybutton.style.display = "none";
-//   }
-// }
-
-// When the user clicks on the button, scroll to the top of the document
-// function topFunction() {
-//   document.body.scrollTop = 0; // For Safari
-//   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-// }
-
-// ===== Scroll to Top ==== 
+// scroll to top
 $(window).scroll(function () {
-  if ($(this).scrollTop() >= 250) {        // If page is scrolled more than 250px
-    $('#back-to-top').fadeIn(200);    // Fade in the arrow
+  // if page is scrolled more than 250px
+  if ($(this).scrollTop() >= 250) {
+    // fade in the arrow
+    $('#back-to-top').fadeIn(200);
   } else {
-    $('#back-to-top').fadeOut(200);   // Else fade out the arrow
+    // else fade out the arrow
+    $('#back-to-top').fadeOut(200);
   }
 });
-$('#back-to-top').click(function () {      // When arrow is clicked
+
+// when arrow is clicked
+$('#back-to-top').click(function () {
+  // scroll to top of body
   $('body,html').animate({
-    scrollTop: 0                       // Scroll to top of body
+    scrollTop: 0
   }, 500);
 });
 
+// go back to home
 $(window).scroll(function () {
-  if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-    $('#return-to-home').fadeIn(200);    // Fade in the arrow
+  // if page is scrolled more than 50px
+  if ($(this).scrollTop() >= 50) {
+    // fade in the arrow
+    $('#return-to-home').fadeIn(200);
   } else {
-    $('#return-to-home').fadeOut(200);   // Else fade out the arrow
+    // else fade out the arrow
+    $('#return-to-home').fadeOut(200);
   }
 });
-$('#return-to-home').click(function () {      // When arrow is clicked
+
+// when arrow is clicked
+$('#return-to-home').click(function () {
+  // scroll to top of body
   $('body,html').animate({
-    scrollTop: 0                       // Scroll to top of body
+    scrollTop: 0
   }, 500);
 });
