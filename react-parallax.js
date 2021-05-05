3f59be94749e43876dda8ae85e233d7cfb974b4e
Marc Oller <marc.om@icloud.com>
2021-05-05 18:11:42 +0200

    Add React script

---
+++ react-parallax.js
@@ -0,0 +1,12 @@
+import ParallaxComponent from 'react-parallax-component';
+
+<ParallaxComponent
+  speed="0.003"
+  width="300"
+  top="40%"
+  left="100"
+>
+  <div>
+    Children component
+  </div>
+</ParallaxComponent>
